9786dc0d47811f9091a1a2f59b7fbcd3cd032cd9
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-08-06 17:23:35 -0700

    Word Search filter should should be processed from db query

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 
 from flask import Flask, request, render_template
 from sqlalchemy import create_engine
@@ -11,31 +12,20 @@ app = Flask(__name__)
 
 Base = declarative_base()
 
-engine = create_engine('sqlite:///database/bibledatabase.db?check_same_thread=False')
+engine = create_engine('sqlite:///database/bibledatabase.db?check_same_thread=False', echo=True)
 
 Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/')
-def root():
-    return "root"
-
-
-@app.route('/word_search', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
         query_param = request.form['word']
-        verses = session.query(Verse).all()
+        verses = session.query(Verse).filter(Verse.verse_string.ilike('%' + query_param + '%'))
 
-        arr = []
-
-        for verse in verses:
-            if query_param in verse.verse_string:
-                arr.append(verse.verse_string)
-
-        return render_template('word_search_result.html', verses=arr)
+        return render_template('word_search_result.html', verses=verses)
 
     else:
         return render_template('search_word.html')
@@ -43,7 +33,7 @@ def search():
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    port = 5000
+    port = int(os.environ.get('PORT', 8000))
     host = '127.0.0.1'
     app.debug = True
     # app.run(host='0.0.0.0', port=5000)

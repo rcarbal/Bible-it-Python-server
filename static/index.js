0c8920de7078b32eb6365dc0a1c63b898e2436d0
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-08-20 05:51:52 +0000

    Exact section should show selected word synonym

--- static/index.js
+++ static/index.js
@@ -1,10 +1,7 @@
-$(document).on('show.bs.modal', '.fade', function (e) {
+$(document).on('show.bs.modal', '.fade', function(e){
     var word = $(this).data('word');
     var pos_list = $(this).data('pos');
     var index = $(this).data('index');
-
-    console.log(pos_list);
-    console.log(index);
 
     pos_list = pos_list.slice(1, pos_list.length -1);
     pos_replaced = pos_list.split(",");
@@ -13,15 +10,25 @@ $(document).on('show.bs.modal', '.fade', function (e) {
     element = this.getElementsByClassName("modal-body")[0];
     element.innerHTML = "";
 
-    // create the definitoin header
+    // create the definition header
     let definitionHeader = document.createElement('h6');
     definitionHeader.innerHTML = "-"+ pos + " " + word;
+
+    // create <hr> tag
+    let hrTag = document.createElement('hr');
+
+
+    // create the synonym header
+    let synonymHeader = document.createElement('h6');
+    synonymHeader.innerHTML = "- Synonyms"
 
     // create ordered list
     let ol = document.createElement('ol');
 
-    element.appendChild(definitionHeader)
-    element.appendChild(ol)
+    element.appendChild(definitionHeader);
+    element.appendChild(ol);
+    element.appendChild(hrTag);
+    element.appendChild(synonymHeader);
 
     axios.get("/api/word/definition?word=" + word)
     .then((response)=>{
@@ -36,14 +43,33 @@ $(document).on('show.bs.modal', '.fade', function (e) {
        });
     });
 
-    getSynonyms(word);
+    // callback
+    const callback = (synonymOrderedList) => {
+        element.appendChild(synonymOrderedList)
+
+    }
+
+    // get synonym ordered list
+    const synOl = getSynonyms(word, callback);
+
+//    element.appendChild(synOl);
+
 
 });
 
-function getSynonyms(word){
+function getSynonyms(word, callback){
     axios.get("/api/word/synonym?word=" + word)
     .then((response) => {
-        console.log(response);
+        let ol = document.createElement('ol');
+
+        response.data.synonyms.forEach((synonym) => {
+            let li = document.createElement('li');
+            li.innerHTML = synonym;
+
+            ol.appendChild(li);
+        });
+
+        callback(ol);
     });
 }
 

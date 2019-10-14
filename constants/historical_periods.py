15c378362823f7c906073d337ad60b5569cf938c
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-10-14 10:16:17 -0700

    Add world historical periods JSON

---
+++ constants/historical_periods.py
@@ -0,0 +1,16 @@
+HISTORICAL_PERIODS = {
+    'prehistory': {
+        'first_year': -3500,
+        'last_year': 300
+    },
+
+    'middle_ages': {
+        'first_year': 476,
+        'last_year': 1492
+    },
+
+    'modern': {
+        'first_year': 1500,
+        'last_year': None
+    }
+}

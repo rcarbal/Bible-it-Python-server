8ccfd69c55f02b4ce09733bbce7beb67a46abd43
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-10-18 23:52:23 -0700

    Refactor biblical Era

--- constants/periods.py
+++ constants/periods.py
@@ -1,56 +1,88 @@
 GENERAL_BIBLICAL_PERIODS = [
+    # Periods of the Bible from Adams synchronological chart
+
+    # The Anti Diluvian Period
+
+    # {
+    #     'position': 1,
+    #     'name': 'The Anti Diluvian',
+    #     'first_year': -4004,
+    #     'last_year': -2348
+    # },
+
     {
         'position': 1,
-        'name': 'Patriarchs',
-        'first_year': -2200,
-        'last_year': -1850
+        'name': 'The Patriarchal Age',
+        'first_year': -4004,
+        'last_year': -1491
     },
+
     {
         'position': 2,
-        'name': 'Sojourn',
-        'first_year': -1850,
-        'last_year': -1450
+        'name': 'The Mosaic Age',
+        'first_year': -1491,
+        'last_year': 33
     },
+
     {
-        'position': 3,
-        'name': 'Wilderness',
-        'first_year': -1450,
-        'last_year': -1400
-    },
-    {
-        'position': 4,
-        'name': 'Conquest',
-        'first_year': -1400,
-        'last_year': -1350
-    },
-    {
-        'position': 5,
-        'name': 'Judges',
-        'first_year': -1350,
-        'last_year': -1050
-    },
-    {
-        'position': 6,
-        'name': 'United Monarchy',
-        'first_year': -1050,
-        'last_year': -930
-    },
-    {
-        'position': 7,
-        'name': 'Divided Monarchy',
-        'first_year': -930,
-        'last_year': -587
-    },
-    {
-        'position': 8,
-        'name': 'Exile',
-        'first_year': -587,
-        'last_year': -539
-    },
-    {
-        'position': 9,
-        'name': 'Pos-exilic period',
-        'first_year': -539,
-        'last_year': -400
+        'position' : 3,
+        'name': 'The Christian Era',
+        'first_year': 33,
+        'last_year': 2019
     }
+
+    # {
+    #     'position': 1,
+    #     'name': 'Patriarchs',
+    #     'first_year': -2200,
+    #     'last_year': -1850
+    # },
+    # {
+    #     'position': 2,
+    #     'name': 'Sojourn',
+    #     'first_year': -1850,
+    #     'last_year': -1450
+    # },
+    # {
+    #     'position': 3,
+    #     'name': 'Wilderness',
+    #     'first_year': -1450,
+    #     'last_year': -1400
+    # },
+    # {
+    #     'position': 4,
+    #     'name': 'Conquest',
+    #     'first_year': -1400,
+    #     'last_year': -1350
+    # },
+    # {
+    #     'position': 5,
+    #     'name': 'Judges',
+    #     'first_year': -1350,
+    #     'last_year': -1050
+    # },
+    # {
+    #     'position': 6,
+    #     'name': 'United Monarchy',
+    #     'first_year': -1050,
+    #     'last_year': -930
+    # },
+    # {
+    #     'position': 7,
+    #     'name': 'Divided Monarchy',
+    #     'first_year': -930,
+    #     'last_year': -587
+    # },
+    # {
+    #     'position': 8,
+    #     'name': 'Exile',
+    #     'first_year': -587,
+    #     'last_year': -539
+    # },
+    # {
+    #     'position': 9,
+    #     'name': 'Pos-exilic period',
+    #     'first_year': -539,
+    #     'last_year': -400
+    # }
 ]

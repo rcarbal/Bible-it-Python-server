276cddc3f824e6e319cc16c74e7fd97478553271
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-10-20 20:48:41 -0700

    timeline.js addClassesToYear() accepts period type to setup timeline

--- static/timeline.js
+++ static/timeline.js
@@ -1,27 +1,37 @@
-function setupHistoricalPeriods(historicalPeriods){
+function setupHistoricalPeriods(historicalPeriods, biblicaPeriods){
     // remove escape characters
     const correctHistoricalPeriods = JSON.parse(historicalPeriods.replace(/&#34;/g,'"'));
 
     // place the correct historical period on the timeline.
     for (h in correctHistoricalPeriods){
         // loop through all the years
-        
+        periodType = "history";
         addClassesToYear(correctHistoricalPeriods[h]['first_year'], 
-            correctHistoricalPeriods[h]['last_year'], correctHistoricalPeriods[h]['name']);
+            correctHistoricalPeriods[h]['last_year'], correctHistoricalPeriods[h]['name'], periodType);
         
     }
 
-    alert("DONE");
-    console.log("DONE");
+    // place the correct biblical periods in the timeline
+    const correctBiblicalPeriods = JSON.parse(biblicaPeriods.replace(/&#34;/g,'"'));
+
+    for (b in correctBiblicalPeriods){
+        
+    }
 }
 
-function addClassesToYear(firstYear, lastYear, period){
-    console.log(`The first year is ${firstYear}, the last year is ${lastYear}`);
+function addClassesToYear(firstYear, lastYear, period, periodType){
+    let type;
+    let paddingType;
+
+    if (periodType == "history"){
+        type = "hist-";
+        paddingType = "hist-pad";
+    }
 
     // find the years element
-    let firstYearElement = document.getElementById(`hist-${firstYear}`);
+    let firstYearElement = document.getElementById(`${type}${firstYear}`);
     firstYearElement.textContent = period;
-    let lastYearElement = document.getElementById(`hist-${lastYear}`);
+    let lastYearElement = document.getElementById(`${type}${lastYear}`);
 
     // add classes to element
     firstYearElement.classList.add("sec-start");
@@ -41,9 +51,9 @@ function addClassesToYear(firstYear, lastYear, period){
         }
 
         // find the years between
-        let yearElement = document.getElementById(`hist-${initialYear}`);
+        let yearElement = document.getElementById(`${type}${initialYear}`);
         yearElement.classList.add('sec');
-        yearElement.classList.add('hist-pad');
+        yearElement.classList.add(paddingType);
 
         initialYear++;
     }

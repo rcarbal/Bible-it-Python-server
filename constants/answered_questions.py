2a0d899bb470947adc39a9b1a59662bea2816a29
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-12-04 15:17:45 -0800

    Add question Holy Spirit presence:God created light Genesis 1:3

--- constants/answered_questions.py
+++ constants/answered_questions.py
@@ -22,6 +22,18 @@ ANSWERED_QUESTION = [
         QUESTION: 'what was the earth like in the beginning?',
         ANSWER: 'The earth was formless and empty.',
         VERSE: 'Genesis 1:2'
+    },
+    {
+        QUESTION: 'was the Holy Spirit present on earth in the beginning?',
+        ANSWER: 'the spirit of God was hovering over the waters before the earth was ordered.',
+        VERSE: 'Genesis 1:2'
+    },
+    {
+        QUESTION: 'how did God create light?',
+        ANSWER: 'God spoke light into existence.',
+        VERSE: 'Genesis 1:3'
     }
+
+
 
 ]
